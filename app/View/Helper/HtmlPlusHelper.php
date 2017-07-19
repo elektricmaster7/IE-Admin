68f0db4e9675596e668c259f087b7a2a0c8f4bb4
João Redondo <geral@inspirelectronics.com>
2017-07-19 15:39:36 +0100

    Language and some form->model binding

--- app/View/Helper/HtmlPlusHelper.php
+++ app/View/Helper/HtmlPlusHelper.php
@@ -5,8 +5,7 @@ class HtmlPlusHelper extends HtmlHelper {
   public $helpers = array('Form', 'Session');
 
   /**
-   * This function extends cakephp's functionality to allow for language
-   * selection in url function
+   * This function extends cakephp's functionality to allow for language selection in url function
    * @method url
    * @author Inspire Electronics
    * @param  mixed   $url  check cakephp documentation *add $url['language'] = 'langCode' for language select
@@ -23,16 +22,21 @@ class HtmlPlusHelper extends HtmlHelper {
   }
 
   /**
-   * Creates a language select element with the apropriate url connections for
-   * a convinent redirect
+   * Creates a language select element with the apropriate url connections for a convinent redirect
+   * IMPORTANT: Intended for use in admin, otherwise you have to create a js function on change to handle the
+   * requests or copy the one in /webroot/admin/admin-material.js
    * @method languageSelect
    * @author Inspire Electronics
    * @param  array   $languages   array of languages, by default pt and en are passed
+   * @param  array   $options     array of options [id, class] for the output select, it uses the same structure as cakephp
+   * @return                      echoes a select box for language switch
    */
-  // TODO: THIS NEEDS A WAY TO DEAL WITH CLASSES AND IDS AND SET LANG
-  public function languageSelect($languages = array('pt'=>'Português', 'en'=>'English'), $default = 'pt'){
+  public function languageSelect($languages = array('pt'=>'Português', 'en'=>'English'), $options = array()){
+    $id = ''; $class = 'language_select';
     if($this->Session->check('Config.language')) $default = $this->Session->read('Config.language');
-    echo $this->Form->input(null, array('options' => $languages, 'default' => $default, 'class' => 'language_select form-control', 'div' => false, 'label' => false));
+    if(isset($options['class'])) $class += ' '.$options['class'];
+    if(isset($options['id'])) $id = $options['id'];
+    echo $this->Form->input(null, array('options' => $languages, 'default' => $default, 'id' => $id,'class' => $class, 'div' => false, 'label' => false));
   }
 
   /**
